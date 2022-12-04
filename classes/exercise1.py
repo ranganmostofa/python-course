9170e7c3e579ff99d5ea672ec13fb538927a361d
Jason Mendez <jasonamendez65@gmail.com>
2022-12-04 16:02:50 -0600

    calculator exercise

--- classes/exercise1.py
+++ classes/exercise1.py
@@ -2,4 +2,18 @@ class Calculator:
     """
     Implement a calculator that does addition and multiplication
     """
-    pass
+    def multiply(self, *args):
+        product = 1
+        for num in args:
+            product *= num
+            print(product)
+        return product
+
+
+    def add(self, *args):
+        return sum(args)
+
+easycalc = Calculator()
+# print(easycalc.multiply(3,4))
+print(easycalc.add(5,10,15,30))
+print(easycalc.multiply(1,2,3,43))
