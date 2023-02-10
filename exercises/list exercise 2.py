971f57862ad1e2c97d35ac2387565447aef306c0
carlosarcedeza <cfa1@rice.edu>
2023-02-10 00:12:59 -0600

    push

--- exercises/list exercise 2.py
+++ exercises/list exercise 2.py
@@ -2,5 +2,5 @@ file_name = "mbox-short.txt"
 
 with open(file_name, "r") as file_obj:
     while line := file_obj.readline():
-        if "From " in line:
+        if line.startswith("From "):
             print(line.split()[1])
